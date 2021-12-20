02d6b13f04dc3ca88cff6853ab12cabb92baa932
bryan mcwatters <bmcwatters@comcast.net>
2021-12-20 14:07:04 -0500

    initial commit

---
+++ OpenWeatherAPI/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace OpenWeatherAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var client = new HttpClient();
+            Console.WriteLine("Please enter your API Key:");
+            var api_Key = Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Please enter the city name: ");
+                var city_name = Console.ReadLine();
+                var weatherURL = $"http://api.openweathermap.org/data/2.5/weather?q={city_name}&units=imperial&appid={api_Key}";
+
+                var response = client.GetStringAsync(weatherURL).Result;
+                var formattedResponse = JObject.Parse(response).GetValue("main").ToString();
+
+                Console.WriteLine("--------------------");
+                Console.WriteLine(formattedResponse);
+                Console.WriteLine();
+
+                Console.WriteLine("Would you like to choose another city?");
+                var userInput = Console.ReadLine();
+                Console.WriteLine();
+                if (userInput.ToLower() == "no")
+                {
+                    break;
+                }
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
